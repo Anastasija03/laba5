653e8c3c3b1d28582d964da03e4f1af2088e4c4e
Anastasija03 <anastasija.kovalevich03@gmail.com>
2022-11-14 14:30:07 +0300

    1

---
+++ precache-manifest.9200a854df9932944be710809548c43d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7803c22cfbc6b568d02cfe1a3dd6bd02",
+    "url": "/red-black-tree-player/index.html"
+  },
+  {
+    "revision": "8606db527db1192b4452",
+    "url": "/red-black-tree-player/static/css/main.19afbe11.chunk.css"
+  },
+  {
+    "revision": "1622863a67b4b37d4cc1",
+    "url": "/red-black-tree-player/static/js/2.7a4422ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/red-black-tree-player/static/js/2.7a4422ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8606db527db1192b4452",
+    "url": "/red-black-tree-player/static/js/main.59e9cd03.chunk.js"
+  },
+  {
+    "revision": "d921f516ddf656e2dd6d",
+    "url": "/red-black-tree-player/static/js/runtime-main.f2124e3f.js"
+  }
+]);
